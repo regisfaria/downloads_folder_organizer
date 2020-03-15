126c6f4ebc23aef8cc14c51aa0eca6c4feaf1413
regisfaria <rgisfaria@yahoo.com.br>
2020-03-15 17:04:02 -0300

    innitial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Downloads Watcher
+
+## Overview 
+  This project is a way to automatic organize you'r PC download's folder .
+  I wish to create a code that will automatic move Pictures into your PC Pictures folder, and PDFs into
+  a PDF folder in the Documents Folder.
+
+  So with this you will have a cleaner Downloads folder and if you have many files in your downloads folder, some files will be easier to find.
+
+## Actual Progress
+Right now what I have is a code that already can move images and pdfs files into the destined folders.
+
+Now I will need to now how to make a way that the code will be executed every time a new file is found in the downloads folder.(first i will look on how to do it on linux, after that i will focus on a windows version)
+
+## Requirements
+
+## Instalation
+
+## Contact
+Email: regisprogramming@gmail.com
+
+[LinkedIn](https://www.linkedin.com/in/regissfaria/) and [GitHub](https://github.com/regisfaria) profiles
---
+++ main.py
@@ -0,0 +1,111 @@
+"""
+ This code tracks a given folder and will transfer any files to a destiny folder
+
+ My goal here is to give a destination whenever a file is found in downloads folder
+and transfer it to a related folder.
+
+--- Exemple cases:
+1. If I download a image, ending with ".jpg", ".jpeg", ".png" I will send it to the Pictures folder
+   A video, to the videos folder
+   and so on...
+
+Must read/watch links:
+
+ The below link will teach me how to use a unix application that runs python code whenever a new file is
+found on a folder:
+https://askubuntu.com/questions/781799/execution-permission-to-all-files-created-under-a-specific-directory-by-default/781909#781909
+
+--------------------------------------------------------
+Next steps:
+[OK] Develop a way to move pictures 
+[OK] Develop a way to move pdfs
+[OK] Wait for files that can be being downloaded, before moving it
+[ ] Find more about how can a code be called everytime we have a new file in the folder
+[ ] Make a installation shell script
+[ ] Make a windows build
+
+
+"""
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from datetime import datetime
+
+import json
+import os
+import time
+import shutil
+
+
+downloads_folder = "/home/regisf/Downloads"
+def type_check(filename):
+    # Folder settings
+    # Set this accordingly with your folder location
+    pictures_folder = "/home/regisf/Pictures"
+    documents_folder = "/home/regisf/Documents"
+    
+    extention = filename.split('.')[-1]
+    
+    # File extention settings
+    # [ ] Add more extentions later
+    img_extentions = ['jpg', 'jpeg', 'png']
+    docs_extentions = ['pdf']
+
+    if extention in img_extentions:
+        new_destination = pictures_folder
+    elif extention in docs_extentions:
+        new_destination = documents_folder
+    # If the file is not on the ext list, I won't move it
+    else:
+        new_destination = False
+
+    return new_destination
+
+def is_downloading(filepath):
+    while True:
+        actual_size = os.stat(filepath)[6]
+        time.sleep(1.5)
+        modified_size = os.stat(filepath)[6]
+        if actual_size == modified_size:
+            return
+    
+##############################################################################################
+
+class Handler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(downloads_folder):
+            #print(filename)
+            new_destination = type_check(filename)
+            # Add move date to the file name
+            new_name = str(datetime.now()).split()[0] + ' - ' + filename
+            
+            # if we will move the file AND the file don't exists on the destiny folder
+            if new_destination and not os.path.isfile(new_destination + "/" + new_name):            
+                # Set paths
+                src = downloads_folder + "/" + filename
+                new_destination += "/" + new_name
+                
+                # Check if the file is downloading
+                is_downloading(src)
+
+                shutil.move(src, new_destination)
+                time.sleep(1)
+                # Sometimes we have a junk file on the old folder, so we will delete it, if it's there
+                try:
+                    os.remove(src)
+                except:
+                    continue
+
+
+##############################################################################################
+event_handler = Handler()
+observer = Observer()
+observer.schedule(event_handler, downloads_folder, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
